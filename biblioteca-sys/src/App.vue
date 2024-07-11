58f7a62efb464803e31c4d32631dfc95f7bf7981
Lucas <super.ghast8@gmail.com>
2024-07-10 23:02:50 -0300

    feat: footer and header to app.vue

--- biblioteca-sys/src/App.vue
+++ biblioteca-sys/src/App.vue
@@ -1,13 +1,34 @@
 <script setup lang="ts">
-
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import  navbarComponent  from "./components/navbarComponent.vue";
+import footerComponent from "./components/footerComponent.vue";
 
 </script>
 
 <template>
-  
+<nav>
+  <navbarComponent></navbarComponent>
+</nav>
 
-  
+  <main>
+    
+  </main>
+
+  <footer>
+    <footerComponent></footerComponent>
+  </footer>
   <RouterView />
 </template>
+
+<style>
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 56px;
+}
+</style>
 
