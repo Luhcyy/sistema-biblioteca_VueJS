cb6e697a4b8de427eab8cb2bfd3affdb04ca236b
Lucas <super.ghast8@gmail.com>
2024-07-10 23:02:03 -0300

    feat: create footer component

---
+++ biblioteca-sys/src/components/footerComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-light custom-bg-dark footer">
+        <div> Design in {{  new Date().getFullYear() }}</div>
+    </div>
+</template>
+
+<style scoped>
+.custom-bg-dark{
+    background-color: #a3130d;
+}
+
+.footer{
+    bottom: 0;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 56px;
+}
+</style>
