5951e42fbc52d7e4019d364228cf041b4dda701c
Lucas <super.ghast8@gmail.com>
2024-07-18 19:54:02 -0300

    feat: imports vue-the-mask

--- biblioteca-sys/src/main.ts
+++ biblioteca-sys/src/main.ts
@@ -2,7 +2,7 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import VueTheMask from 'vue-the-mask'
 
 import App from './App.vue'
 import router from './router'
@@ -11,6 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(VueTheMask)
 
 app.mount('#app')
